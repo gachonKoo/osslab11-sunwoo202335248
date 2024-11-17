977ba5e90f0346196bd46bf4deb21480db7219c2
sunwoo202335248 <sunwoo2528@gachon.ac.kr>
2024-11-17 23:50:42 +0900

    Fix code for Autograding tests

--- tester.py
+++ tester.py
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-import geo.utils as utils
-=======
-import sys
-import os
-
-# Add the current directory to Python path
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
 import geo.utils as utils
 
->>>>>>> 3323d06 (Add project files)
 # Calculate the length of the hypotenuse (c) when a=3 and b=4
 a, b = 3, 4
 c = utils.pythagoras(a, b)
